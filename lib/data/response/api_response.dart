4f12aabea6791ae4d7c9e84f37adf9767bcd820e
KAPIL SHARMA <140537615+kapilsharma420@users.noreply.github.com>
2025-03-21 10:21:35 +0530

    first commit

---
+++ lib/data/response/api_response.dart
@@ -0,0 +1,18 @@
+import 'package:mvvm/data/response/status.dart';
+
+class ApiResponse<T> {
+  Status? status;
+  T? data;
+  String? message;
+  ApiResponse(this.status, this.data, this.message);
+  ApiResponse.loading() : status = Status.LOADING;
+
+  ApiResponse.completed() : status = Status.COMPLETED;
+
+  ApiResponse.error() : status = Status.ERROR;
+
+  @override
+  String tostring() {
+    return "status: $status \n message: $message \n data: $data";
+  }
+}
