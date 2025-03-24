c7327360e722c85c1d0cc3cf9a752d6759eb8236
KAPIL SHARMA <140537615+kapilsharma420@users.noreply.github.com>
2025-03-24 20:14:19 +0530

    changes

---
+++ lib/data/network/network_api_services.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:mvvm/data/app_excepsion.dart';
+import 'package:mvvm/data/network/base_api_services.dart';
+
+class NetworkApiServices extends BaseApiServices {
+  @override
+  Future getGetApiResponse(String url) async {
+    dynamic responseJson;
+    try {
+      final Response = await http
+          .get(Uri.parse('uri'))
+          .timeout(Duration(seconds: 10));
+      responseJson = returnResponse(Response);
+    } on SocketException {
+      throw FetchDataException(' no internet connection ');
+    }
+    return responseJson;
+  }
+
+  @override
+  Future getPostApiResponse(String url, dynamic data) async {
+    dynamic responseJson;
+    try {
+      Response response = await post(
+        Uri.parse(url),
+        body: data,
+      ).timeout(Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException(' no internet connection ');
+    }
+    return responseJson;
+  }
+
+  dynamic returnResponse(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        dynamic responseJson = jsonDecode(response.body);
+        return responseJson;
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 500:
+      case 404:
+        throw UnauthorisedException(response.body.toString());
+      default:
+        throw FetchDataException(
+          'error occured while communicating with server ' +
+              'with status code' +
+              response.statusCode.toString(),
+        );
+    }
+  }
+}
