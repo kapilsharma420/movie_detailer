bfa365cd9ee17639bd4074a5f8abcda377f80f40
KAPIL SHARMA <140537615+kapilsharma420@users.noreply.github.com>
2025-03-25 12:17:56 +0530

    chnages

---
+++ lib/view/home_sceen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeSceen extends StatefulWidget {
+  const HomeSceen({super.key});
+
+  @override
+  State<HomeSceen> createState() => _HomeSceenState();
+}
+
+class _HomeSceenState extends State<HomeSceen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
